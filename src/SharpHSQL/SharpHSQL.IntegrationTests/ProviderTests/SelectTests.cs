11fb1e97c4dcbc75455d988691789c1b5a725fc9
Sergey Gorbunov <svgorbunov83@yandex.ru>
2016-06-19 19:21:22 +0300

    Basic refactoring tests completed.

--- src/SharpHSQL/SharpHSQL.IntegrationTests/ProviderTests/SelectTests.cs
+++ src/SharpHSQL/SharpHSQL.IntegrationTests/ProviderTests/SelectTests.cs
@@ -7,9 +7,8 @@ namespace SharpHSQL.IntegrationTests.ProviderTests {
     [TestFixture]
     class SelectTests : BaseQueryTest {
         [Test]
-        [Ignore("Not correct")]
-        public void T2() {
-            TestQuery((connection) => {
+        public void SimpleSelect() {
+            TestQuery(connection => {
                 var cmd = new SharpHsqlCommand("", connection);
                 cmd.CommandText = "SELECT * FROM \"books\"";
                 var reader = cmd.ExecuteReader();
@@ -17,89 +16,63 @@ namespace SharpHSQL.IntegrationTests.ProviderTests {
                 var booksCount = 0;
                 while (reader.Read()) {
                     booksCount += 1;
-                    //Console.WriteLine("id={0}book={1},\tauthor={2},\tqty={3},\tvalue={4}", reader.GetInt32(0),
-                    //    reader.GetString(1), reader.GetString(2), reader.GetInt32(3), reader.GetDecimal(4));
                 }
 
-                Assert.AreEqual(3, booksCount);
+                Assert.AreEqual(4, booksCount);
             });
         }
 
         [Test]
-        [Ignore("Not correct")]
-        public void T3() {
+        public void SelectWithOrder() {
             TestQuery(connection => {
                 var cmd = new SharpHsqlCommand("", connection);
                 cmd.CommandText = "SELECT * FROM \"books\" ORDER BY \"value\"";
                 var reader = cmd.ExecuteReader();
 
+                var expectedOrder = new[] { 4, 1, 3, 2 };
+
+                var rowIndex = 0;
                 while (reader.Read()) {
-                    Console.WriteLine("id={0}book={1},\tauthor={2},\tqty={3},\tvalue={4}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3), reader.GetDecimal(4));
+                    Assert.AreEqual(expectedOrder[rowIndex], reader.GetInt32(0));
+                    rowIndex += 1;
                 }
 
-                Console.WriteLine();
-
                 reader.Close();
-
-                Console.WriteLine();
-                Assert.Pass();
             });
         }
 
         [Test]
-        [Ignore("Not correct")]
-        public void T4() {
+        public void WhereClauseTest() {
             TestQuery(connection => {
                 var cmd = new SharpHsqlCommand("", connection);
                 cmd.CommandText = "SELECT COUNT(*) as CNT, SUM(\"value\") FROM \"books\" WHERE \"author\" = 'Andy'";
                 var reader = cmd.ExecuteReader();
+                reader.Read();
 
-                while (reader.Read()) {
-                    Console.WriteLine("count={0},\tvalue={1}", reader.GetInt32(0), reader.GetDecimal(1));
-                }
-
-                Console.WriteLine();
-
+                Assert.AreEqual(2, reader.GetInt32(0));
+                Assert.AreEqual(81.15, reader.GetDecimal(1));
                 reader.Close();
-                Assert.Pass();
             });
         }
 
         [Test]
-        [Ignore("Not correct")]
-        public void T5() {
+        public void GroupByClauseTest() {
             TestQuery(connection => {
                 var cmd = new SharpHsqlCommand("", connection);
-                cmd.CommandText = "SELECT \"name\", \"author\", SUM(\"value\") FROM \"books\" WHERE \"author\" = 'Andy' GROUP BY \"name\", \"author\";";
+                cmd.CommandText = "SELECT \"author\", SUM(\"value\") FROM \"books\" GROUP BY \"author\";";
                 var reader = cmd.ExecuteReader();
 
-                while (reader.Read()) {
-                    Console.WriteLine("name={0},\tauthor={1},\tvalue={2}", reader.GetString(0), reader.GetString(1), reader.GetDecimal(2));
-                }
+                reader.Read();
+                Assert.AreEqual("Andy", reader.GetString(0));
+                Assert.AreEqual(81.15, reader.GetDecimal(1));
 
-                Console.WriteLine();
+                reader.Read();
+                Assert.AreEqual("Any", reader.GetString(0));
+                Assert.AreEqual(34.0, reader.GetDecimal(1));
 
-                reader.Close();
-                Assert.Pass();
-            });
-        }
-
-        [Test]
-        [Ignore("Not correct")]
-        public void T6() {
-            TestQuery(connection => {
-                var cmd = new SharpHsqlCommand("", connection);
-                cmd.CommandText = "SELECT \"name\", SUM(\"value\") FROM \"books\" WHERE \"author\" = 'Andy' GROUP BY \"name\";";
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read()) {
-                    Console.WriteLine("name={0},\tvalue={1}", reader.GetString(0), reader.GetDecimal(1));
-                }
-
-                Console.WriteLine();
+                Assert.False(reader.Read());
 
                 reader.Close();
-                Assert.Pass();
             });
         }
 
@@ -115,9 +88,11 @@ namespace SharpHSQL.IntegrationTests.ProviderTests {
                 cmd = new SharpHsqlCommand("", connection);
                 cmd.CommandText = "INSERT INTO \"books\" VALUES (1, 'Book000', 'Any', 1, '23.5');";
                 cmd.ExecuteNonQuery();
-                cmd.CommandText = "INSERT INTO \"books\" VALUES (2, 'Book001', 'Andy2', 2, '43.9');";
+                cmd.CommandText = "INSERT INTO \"books\" VALUES (2, 'Book001', 'Andy', 2, '43.9');";
                 cmd.ExecuteNonQuery();
                 cmd.CommandText = "INSERT INTO \"books\" VALUES (3, 'Book002', 'Andy', 3, '37.25');";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "INSERT INTO \"books\" VALUES (4, 'Book003', 'Any', 1, '10.5');";
                 cmd.ExecuteNonQuery();
             }
             tran.Commit();
