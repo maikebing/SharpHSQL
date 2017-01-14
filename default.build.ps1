04c16d16bed14d54421c09e6fa6d081b43ab8db4
Sergey Gorbunov <svgorbunov83@yandex.ru>
2017-01-14 23:35:54 +0300

    Replacement of Psake on a Invoke-Build.

---
+++ default.build.ps1
@@ -0,0 +1,40 @@
+use 14.0 MSBuild
+
+cls
+task . Tests
+
+task Restore {
+    & .\.paket\paket.bootstrapper.exe
+    & .\.paket\paket.exe restore
+}
+
+# Synopsis: Cleaning.
+task Clean {
+    if (Test-Path(".tests")) {  
+        rm -r .tests/
+    }
+    
+    rm mytest.*
+    & git checkout src/SharpHSQL/SharpHSQL/doc
+}
+
+# Synopsis: Build the debug version.
+task Build {
+    exec { msbuild /v:q /nologo src\SharpHSQL\SharpHSQL.sln }
+    # & "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /v:q /nologo src\SharpHSQL\SharpHSQL.sln
+}
+
+# Synopsis: Building and running tests.
+task Tests Clean, Restore, Build, {
+    & packages\NUnit.ConsoleRunner\tools\nunit3-console.exe src\SharpHSQL\SharpHSQL.IntegrationTests\bin\Debug\SharpHSQL.IntegrationTests.dll
+}
+
+# Synopsis: Building for Mono platform (using xbuild).
+task Mono {
+    & xbuild /v:q /nologo src\SharpHSQL\SharpHSQL.sln
+}
+
+# Synopsis: Analysis of test coverage. Required dotCover.
+task Cover Tests, {
+    & tools\dotCover.2016.1\dotcover analyse tools\coverage.xml    
+}
